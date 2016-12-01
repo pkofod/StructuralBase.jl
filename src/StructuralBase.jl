f77e800ea4db9604ba36f6b0613e688c1e07acfa
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2016-12-01 22:01:50 +0100

    Add first draft, even if it has a lot of noise.

--- src/StructuralBase.jl
+++ src/StructuralBase.jl
@@ -1,5 +1,66 @@
 module StructuralBase
 
-# package code goes here
+using Optim, MDPTools
+const AbstractUtility = MDPTools.AbstractUtility
+const AbstractState = MDPTools.AbstractState
+export EstimationMethod, EstimationResults, AbstractUtility,
+        AbstractTrace, ConvergenceInfo
+
+abstract EstimationMethod
+abstract ConvergenceInfo
+
+# --- AbstractTrace ---
+
+abstract AbstractTrace
+abstract Trace
+
+
+TraceNFXP(K, n_var) = TraceNFXP(zeros(K, n_var),
+                                zeros(K),
+                                zeros(K),
+                                zeros(K),
+                                zeros(K))
+
+type EstimationResults{T<:EstimationMethod}
+    E::T
+    conv::ConvergenceInfo
+    trace::Optim.MultivariateOptimizationResults
+end
+
+
+type EstimationResultsNPL
+    E::EstimationMethod
+    conv::ConvergenceInfo
+    trace
+end
+
+type ConvergenceNPL <: ConvergenceInfo
+    maxK::Int64
+    # Outer loop
+    flag::Bool
+    outer::Int64
+    norm_θ::Float64
+    # Inner loop
+    ll::Float64
+    norm_grad::Float64
+    iter_maxlike::Int64
+end
+
+ConvergenceNPL() = ConvergenceNPL(0, false, 0, 0., 0., 0., 0)
+
+
+type TraceNPL <: Trace
+    θ::Matrix{Float64}
+    norm_Δθ::Vector{Float64}
+    ll::Vector{Float64}
+    Δll::Vector{Float64}
+    norm_g::Vector{Float64}
+end
+
+TraceNPL(K, n) = TraceNPL(zeros(K, n),
+                          zeros(K),
+                          zeros(K),
+                          zeros(K),
+                          zeros(K))
 
 end # module
