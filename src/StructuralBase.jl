92574f3d7f2b3f8623b7297edb9c7bee095e1fcf
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2017-02-03 11:25:20 +0100

    Improve results and output.

--- src/StructuralBase.jl
+++ src/StructuralBase.jl
@@ -6,9 +6,10 @@ const AbstractUtility = MDPTools.AbstractUtility
 const AbstractState = MDPTools.AbstractState
 export EstimationMethod, EstimationResults, AbstractUtility,
         AbstractTrace, ConvergenceInfo, LinearUtility,
-        AbstractState
+        AbstractState, hessian, gradient, loglikelihood
 
 import StatsBase: loglikelihood
+import Base: gradient
 
 
 abstract EstimationMethod
@@ -29,12 +30,15 @@ TraceNFXP(K, n_var) = TraceNFXP(zeros(K, n_var),
 type EstimationResults{T<:EstimationMethod, Tf<:Real}
     E::T
     loglikelihood::Tf
+    ∇loglikelihood::Vector{Tf}
     ∇²loglikelihood::Matrix{Tf}
+    tdf::Optim.TwiceDifferentiableFunction #TwiceDifferentiableFunction
+    coef::Vector{Tf}
     conv::ConvergenceInfo
     trace::Any#::Optim.MultivariateOptimizationResults FIXME should be MultivariateOptimizationResults
-    std_err
+    nobs
+    meta
 end
-
 
 type ConvergenceNPL <: ConvergenceInfo
     maxK::Int64
@@ -65,9 +69,29 @@ TraceNPL(K, n) = TraceNPL(zeros(K, n),
                           zeros(K),
                           zeros(K))
 
-nobs(res) = res.E.D.nobs
+nobs(res) = res.nobs
 loglikelihood(res) = res.loglikelihood
-hessian(res::EstimationResults) = res.∇²loglikelihood*nobs(res)
+loglikelihood(res, x) = -res.tdf.f(x)*nobs(res)
+gradient(res) = res.∇loglikelihood
+function gradient(res, x)
+    g = similar(x)
+    res.tdf.g!(x, g)
+    -g*nobs(res)
+end
+hessian(res::EstimationResults) = res.∇²loglikelihood
+function hessian(res::EstimationResults, x)
+    n = length(x)
+    h = zeros(n, n)
+    res.tdf.h!(x, h)
+    -h*nobs(res)
+end
+coef(res::EstimationResults) = res.coef
+coef(res, i) = coef(res)[i]
 vcov(res::EstimationResults) = inv(-hessian(res))
 stderr(res::EstimationResults) = sqrt(diag(vcov(res)))
+stderr(res::EstimationResults, i) = sqrt(diag(vcov(res)))[i]
+tstat(res, i, x) = (coef(res, i)-x)/(stderr(res, i))
+tstat(res, i) = tstat(res, i, 0)
+tstat(res, x::Vector) = [tstat(res, i, x[i]) for i = 1:length(x)]
+tstats(res) = tstat(res, zeros(coef(res)))
 end # module
